40ac3e8c266f18aaccf5dad39a5ece3f2795acdd
CrossNick <nagatopeih@yandex.ru>
2017-07-03 13:24:25 +0300

    Added

---
+++ Assets/Spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public float speed = 3.0f;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, speed, 0);
+	}
+}
